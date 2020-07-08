86f41eed1c0353cffe6b42c109fce2f41e999e1e
ShosukeDoi <doiverson.canada@gmail.com>
2020-07-08 15:34:56 -0700

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from "./Header";
 
 function App() {
   const themeHook = useState("light");
-  console.log(themeHook)
   return (
       <ThemeContext.Provider value={themeHook}>
           <Header/>
--- src/MainWithClass.js
+++ src/MainWithClass.js
@@ -1,4 +1,4 @@
-import React, {Component, useContext} from "react";
+import React, { Component, useContext, useReducer } from "react";
 import ThemeContext from "./Context/ThemeContext";
 import AppTheme from "./Colors"
 
@@ -26,54 +26,64 @@ import AppTheme from "./Colors"
 //
 // }
 
-class Main extends Component {
-    constructor() {
-        super();
-        this.state = {
-        }
-    }
-    render(){
-        return(
-            <ThemeContext.Consumer>
-                {
-                    ([theme]) => {
-                        const currentTheme = AppTheme[theme];
-                        return(
-                            <main style = {{
-                                padding: "1rem",
-                                backgroundColor: `${currentTheme.backgroundColor}`,
-                                color: `${currentTheme.textColor}`,
-                            }}>
-                                <h1>Heading 1</h1>
-                                <p>This is a paragraph</p>
-                                <button> This is a button</button>
-                            </main>
-                        )
+// class Main extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//         }
+//     }
+//     render(){
+//         return(
+//             <ThemeContext.Consumer>
+//                 {
+//                     ([theme]) => {
+//                         const currentTheme = AppTheme[theme];
+//                         return(
+//                             <main style = {{
+//                                 padding: "1rem",
+//                                 backgroundColor: `${currentTheme.backgroundColor}`,
+//                                 color: `${currentTheme.textColor}`,
+//                             }}>
+//                                 <h1>Heading 1</h1>
+//                                 <p>This is a paragraph</p>
+//                             </main>
+//                         )
+//                     }
+//                 }
+//             </ThemeContext.Consumer>
+//         );
+//     }
+//
+// }
 
-                    }
-                }
-            </ThemeContext.Consumer>
-        );
-    }
-
+const reducer = ( state, action ) => {
+    if (action === 'INCREMENT') return {count: state.count + 1};
+    else return {count: state.count - 1};
 }
 
-// const Main = () => {
-//     const theme = useContext(ThemeContext)[0];
-//     console.log(theme)
-//     const currentTheme = AppTheme[theme];
-//     return(
-//         <main style = {{
-//             padding: "1rem",
-//             backgroundColor: `${currentTheme.backgroundColor}`,
-//             color: `${currentTheme.textColor}`,
-//
-//         }}>
-//             <h1>Heading 1</h1>
-//             <p>This is a paragraph</p>
-//             <button> This is a button</button>
-//         </main>
-//     );
-// }
+const Main = () => {
+    const theme = useContext(ThemeContext)[0];
+    const currentTheme = AppTheme[theme];
+
+    const initialState = {
+        count: 0
+    }
+    const [ state, dispatch ] = useReducer(reducer, initialState)
+
+    return(
+        <main style = {{
+            padding: "1rem",
+            backgroundColor: `${currentTheme.backgroundColor}`,
+            color: `${currentTheme.textColor}`,
+
+        }}>
+            <h1>Heading 1</h1>
+            <p>This is a paragraph</p>
+            <button onClick={() => dispatch('INCREMENT')}>+</button>
+            <button>{state.count}</button>
+            <button onClick={() => dispatch('DECREMENT')}>-</button>
+           </main>
+    );
+}
 
 export default Main;
